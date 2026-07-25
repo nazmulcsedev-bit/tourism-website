32ea81c0cafc85f96cbb3a26d32f2696e85a135d
nazmulcsedev <nazmul.csedev@gmail.com>
2026-07-26 02:11:26 +0600

    dfsdf

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,41 +1,20 @@
-// import axios from 'axios';
-// const baseURL = import.meta.env.VITE_API_URL || '/api';
-
-// const api = axios.create({
-//   baseURL,
-// });
-
-// // Attach JWT token to every request if user is logged in
-// api.interceptors.request.use((config) => {
-//   const userInfo = localStorage.getItem('userInfo');
-//   if (userInfo) {
-//     const { token } = JSON.parse(userInfo);
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//   }
-//   return config;
-// });
-
-// export default api;
-// import axios from "axios";
-
-// console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
-// api.interceptors.request.use((config) => {
-//   console.log("Final Request URL:", config.baseURL + config.url);
-//   return config;
-// });
-
-// export default api;
-import axios from "axios";
+import axios from 'axios';
+const baseURL = import.meta.env.VITE_API_URL || '/api';
 
 const api = axios.create({
-  baseURL: "https://tourism-website-as0c.onrender.com/api",
+  baseURL,
+});
+
+// Attach JWT token to every request if user is logged in
+api.interceptors.request.use((config) => {
+  const userInfo = localStorage.getItem('userInfo');
+  if (userInfo) {
+    const { token } = JSON.parse(userInfo);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  return config;
 });
 
 export default api;
